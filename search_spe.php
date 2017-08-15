5dda60588ea9377d8f3e8f655387d9889f231c73
faeizt <31051184+faeizt@users.noreply.github.com>
2017-08-16 07:08:32 +0800

    Add files via upload

---
+++ search_spe.php
@@ -0,0 +1,59 @@
+<link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/plug-ins/a5734b29083/integration/bootstrap/3/dataTables.bootstrap.css">
+<link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/responsive/1.0.1/css/dataTables.responsive.css">
+<style type="text/css">
+    table.dataTable th,
+    table.dataTable td {
+        white-space: nowrap;
+    }
+    .row{
+        border:1px solid black;
+}	
+</style>
+<table id="media_tbl" class="table table-striped table-hover dt-responsive">
+	<thead><tr><td>Nama Media</td><td>header</td><td>header</td><td>header</td><td>header</td></tr></thead>
+	<tbody>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+		<tr><td>data</td><td>header</td><td>header</td><td>header</td><td>header</td></tr>
+  	</tbody>
+	<tfoot><tr><td>footer</td><td>header</td><td>header</td><td>header</td><td>header</td></tr></tfoot>
+</table>
+<script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="http://cdn.datatables.net/responsive/1.0.1/js/dataTables.responsive.min.js"></script>
+<script type="text/javascript" src="http://cdn.datatables.net/plug-ins/a5734b29083/integration/bootstrap/3/dataTables.bootstrap.js"></script>
+<script type="text/javascript">
+	$('#media_tbl').dataTable({
+        // parameters
+});
+
+</script>
